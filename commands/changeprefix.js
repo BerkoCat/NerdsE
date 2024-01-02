252a98f9ee20e75fc6702879b175815c6ad143fc
Berkooo <95771312+BerkoCat@users.noreply.github.com>
2024-01-02 15:21:25 +0700

    Create changeprefix.js

---
+++ commands/changeprefix.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: 'changeprefix',
+  description: 'Change the bot\'s prefix for this server',
+  execute(message, args, serverPrefixes, DEFAULT_PREFIX) {
+    if (!message.member.permissions.has('ADMINISTRATOR')) {
+      return message.reply('You do not have the required permissions to use this command.');
+    }
+
+    if (!args.length) {
+      return message.reply(`Please provide a new prefix. To reset to the default prefix (${DEFAULT_PREFIX}), use "${DEFAULT_PREFIX}" as the new prefix.`);
+    }
+
+    if (args[0].toLowerCase() === 'default') {
+      serverPrefixes.delete(message.guild.id);
+      return message.reply(`Prefix has been reset to the default: ${DEFAULT_PREFIX}`);
+    }
+
+    const newPrefix = args[0];
+    serverPrefixes.set(message.guild.id, newPrefix);
+
+    return message.reply(`Prefix has been updated to ${newPrefix}`);
+  },
+};
